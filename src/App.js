a6278ca3648cbef34459a574fd6e15ecfa5a0e2c
ww57403 <ww57403@naver.com>
2021-01-08 17:12:51 +0900

    모던리엑트 10. 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
+  const name = "hello";
+  const style = {
+    backgroundColor: 'black'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div style={style}>{name}</div>
+      <InputSample/>
+     {/* <Counter/> */}
     </div>
   );
 }
